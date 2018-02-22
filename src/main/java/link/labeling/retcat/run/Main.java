14a057aed2f3eb7a1c0d8ee6401959fb113bdc8f
Florian Thiery <web@florian-thiery.de>
2018-02-22 12:03:57 +0100

    delete unused things from main class

--- src/main/java/link/labeling/retcat/run/Main.java
+++ src/main/java/link/labeling/retcat/run/Main.java
@@ -9,43 +9,16 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.sql.SQLException;
-import java.util.Map;
 import link.labeling.retcat.exceptions.ResourceNotAvailableException;
-import link.labeling.retcat.utils.RetcatUtils;
-import link.labeling.retcat.utils.Retcat_Getty;
-import link.labeling.retcat.utils.SuggestionItem;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
 
-/**
- * main class
- *
- * @author thiery
- */
 public class Main {
 
-    /**
-     * main method
-     *
-     * @param args
-     * @throws IOException
-     * @throws SQLException
-     */
     public static void main(String[] args) throws IOException, SQLException, ResourceNotAvailableException, ParseException {
         String output = "";
         try {
             output = POM_retcat.getInfo().toJSONString() + "\r\n";
-        } catch (Exception e) {
-            output = Logging.getMessageJSON(e, "link.labeling.retcat.run.Main") + "\r\n";
-            System.out.println(Logging.getMessageJSON(e, "link.labeling.retcat.run.Main"));
-        }
-        try {
-            //output = Retcat_Getty.queryAAT("gold").toJSONString()  + "\r\n";
-            //output = Retcat_Getty.queryTGN("Mainz").toJSONString()  + "\r\n";
-            //output = Retcat_Getty.queryULAN("Dal").toJSONString()  + "\r\n";
-            output = Retcat_Getty.info("http://vocab.getty.edu/aat/300266417").toJSONString() + "\r\n";
-        } catch (Exception e) {
+        } catch (IOException e) {
             output = Logging.getMessageJSON(e, "link.labeling.retcat.run.Main") + "\r\n";
             System.out.println(Logging.getMessageJSON(e, "link.labeling.retcat.run.Main"));
         }
