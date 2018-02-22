de87d0bd8e8c89ffae980918b81dd6ec9747479a
Florian Thiery <web@florian-thiery.de>
2018-02-22 10:40:40 +0100

    add exceptions

---
+++ src/main/java/link/labeling/retcat/exceptions/ResourceNotAvailableException.java
@@ -0,0 +1,12 @@
+package link.labeling.retcat.exceptions;
+
+public class ResourceNotAvailableException extends Exception {
+
+	public ResourceNotAvailableException(String message) {
+		super(message);
+	}
+	
+	public ResourceNotAvailableException() {
+		super();
+	}
+}
---
+++ src/main/java/link/labeling/retcat/exceptions/RetcatException.java
@@ -0,0 +1,13 @@
+package link.labeling.retcat.exceptions;
+
+public class RetcatException extends Exception {
+	
+	public RetcatException(String message) {
+        super(message);
+    }
+	
+	public RetcatException() {
+        super();
+    }
+	
+}
