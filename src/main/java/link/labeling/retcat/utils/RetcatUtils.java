e5b07613bd817e6c4c8074e69256573d712db86a
Florian Thiery <web@florian-thiery.de>
2018-02-26 10:29:13 +0100

    add encodeURIComponent

--- src/main/java/link/labeling/retcat/utils/RetcatUtils.java
+++ src/main/java/link/labeling/retcat/utils/RetcatUtils.java
@@ -1,5 +1,7 @@
 package link.labeling.retcat.utils;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import link.labeling.retcat.classes.SuggestionItem;
 import java.util.Iterator;
 import java.util.Map;
@@ -85,6 +87,22 @@ public class RetcatUtils {
             }
         }
         return outArray;
+    }
+
+    public static String encodeURIComponent(String s) {
+        String result;
+        try {
+            result = URLEncoder.encode(s, "UTF-8")
+                    .replaceAll("\\+", "%20")
+                    .replaceAll("\\%21", "!")
+                    .replaceAll("\\%27", "'")
+                    .replaceAll("\\%28", "(")
+                    .replaceAll("\\%29", ")")
+                    .replaceAll("\\%7E", "~");
+        } catch (UnsupportedEncodingException e) {
+            result = s;
+        }
+        return result;
     }
 
 }
