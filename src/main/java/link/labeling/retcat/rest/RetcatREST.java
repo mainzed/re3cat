07852b1e4b98ca37bb4971b5f8c435c823de116c
Florian Thiery <web@florian-thiery.de>
2018-02-26 10:33:09 +0100

    add chronontology to rest

--- src/main/java/link/labeling/retcat/rest/RetcatREST.java
+++ src/main/java/link/labeling/retcat/rest/RetcatREST.java
@@ -10,6 +10,7 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import link.labeling.retcat.log.Logging;
+import link.labeling.retcat.queries.Retcat_ChronOntology;
 import link.labeling.retcat.queries.Retcat_Getty;
 import link.labeling.retcat.queries.Retcat_HeritageData;
 
@@ -149,6 +150,38 @@ public class RetcatREST {
 	public Response getInfoHeritageData(@QueryParam("uri") String uri) {
 		try {
 			JSONObject jsonOut = Retcat_HeritageData.info(uri);
+			return Response.ok(jsonOut).header("Content-Type", "application/json;charset=UTF-8").build();
+		} catch (Exception e) {
+			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(Logging.getMessageJSON(e, "link.labeling.retcat.utils.RetcatREST"))
+					.header("Content-Type", "application/json;charset=UTF-8").build();
+		}
+	}
+    
+    @GET
+	@Path("/query/chronontology")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=UTF-8")
+	public Response getQueryResultsChronOntology(@QueryParam("query") String searchword) {
+		try {
+			JSONArray outArray = new JSONArray();
+			if (searchword.startsWith("uri:")) {
+				outArray.add(Retcat_ChronOntology.info(searchword.replace("uri:", "")));
+				outArray.remove(new JSONObject());
+			} else {
+				outArray = Retcat_ChronOntology.query(searchword);
+			}
+			return Response.ok(outArray).header("Content-Type", "application/json;charset=UTF-8").build();
+		} catch (Exception e) {
+			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(Logging.getMessageJSON(e, "link.labeling.retcat.utils.RetcatREST"))
+					.header("Content-Type", "application/json;charset=UTF-8").build();
+		}
+	}
+
+	@GET
+	@Path("/info/chronontology")
+	@Produces(MediaType.APPLICATION_JSON + ";charset=UTF-8")
+	public Response getInfoChronOntology(@QueryParam("uri") String uri) {
+		try {
+			JSONObject jsonOut = Retcat_ChronOntology.info(uri);
 			return Response.ok(jsonOut).header("Content-Type", "application/json;charset=UTF-8").build();
 		} catch (Exception e) {
 			return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(Logging.getMessageJSON(e, "link.labeling.retcat.utils.RetcatREST"))
