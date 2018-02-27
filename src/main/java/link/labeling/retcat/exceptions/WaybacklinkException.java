65af25c5f5ad81af466606acbd826800d32e0b7c
Florian Thiery <web@florian-thiery.de>
2018-02-27 10:25:39 +0100

    add WaybacklinkException

---
+++ src/main/java/link/labeling/retcat/exceptions/WaybacklinkException.java
@@ -0,0 +1,13 @@
+package link.labeling.retcat.exceptions;
+
+public class WaybacklinkException extends Exception {
+
+	public WaybacklinkException(String message) {
+        super(message);
+    }
+	
+	public WaybacklinkException() {
+        super();
+    }
+}
+
